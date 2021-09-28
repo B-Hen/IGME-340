f8f847531be7b2af5bee30eb632d70e065005673
Breanna Henriquez <50420371+B-Hen@users.noreply.github.com>
2021-09-28 17:53:52 -0400

    Finished exercise 6

---
+++ Paging/Paging/Paging/ViewModels/DetailPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Paging.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paging.ViewModels
+{
+    public class DetailPageViewModel
+    {
+        //properties
+        public GameData Data { get; set; }
+        public DetailPageViewModel(GameData data)
+        {
+            Data = data;
+        }
+    }
+}
