ea5d0a0e197ecddb6aee2a4442c1cc302bf3e42a
Breanna Henriquez <50420371+B-Hen@users.noreply.github.com>
2021-10-14 03:20:45 -0400

    Started project 1, added image, basic UI, and click events

---
+++ CraneClikcer/CraneClikcer/CraneClikcer/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CraneClikcer
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            int score = 0;
+
+            //check for taps on the crane image
+            var tapGestureRecognizer = new TapGestureRecognizer();
+            tapGestureRecognizer.Tapped += async(s,e) =>
+            {
+                score++;
+                Score.Text = "Score: " + score;
+            };
+
+            craneImage.GestureRecognizers.Add(tapGestureRecognizer);
+        }
+    }
+}
